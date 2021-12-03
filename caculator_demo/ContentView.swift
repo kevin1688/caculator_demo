d5501489e79ef3da2f07507c7aa67bb7a59d9976
kevin1688 <jackexi@gmail.com>
2021-12-03 21:16:44 +0800

    Initial Commit

---
+++ caculator_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  caculator_demo
+//
+//  Created by kai wen chen on 2021/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ caculator_demo/caculator_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  caculator_demoApp.swift
+//  caculator_demo
+//
+//  Created by kai wen chen on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct caculator_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
